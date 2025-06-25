1342fd8a0b357e645d4e2abbc10cc2b6f85b7047
bin5003 <72881828+bin5003@users.noreply.github.com>
2025-06-25 15:56:30 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const loginForm = document.querySelector('#login-form'); // login-from
+const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
+const greeting = document.querySelector('#greeting');
 
-const link = document.querySelector('a');
+const HIDDEN_CLASSNAME = 'hidden';
 
-function onLoginSubmit(information) {
-  information.preventDefault();
-  console.log(loginInput.value);
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  // greeting.innerText = 'Hello ' + username;
+  greeting.innerText = `hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 function handleLinkClick(event) {
@@ -14,4 +19,3 @@ function handleLinkClick(event) {
 }
 
 loginForm.addEventListener('submit', onLoginSubmit);
-link.addEventListener('click', handleLinkClick);
