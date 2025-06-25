4be95e0f34449d2a8a008bd1b6f690b71296014a
bin5003 <72881828+bin5003@users.noreply.github.com>
2025-06-25 14:50:44 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-const loginForm = document.querySelector('#login-form');
-const loginInput = document.querySelector('#login-from input');
+const loginForm = document.querySelector('#login-form'); // login-from
+const loginInput = document.querySelector('#login-form input');
+
+const link = document.querySelector('a');
 
 function onLoginSubmit(information) {
   information.preventDefault();
-  console.log(information);
+  console.log(loginInput.value);
+}
+
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.dir(event);
 }
 
 loginForm.addEventListener('submit', onLoginSubmit);
+link.addEventListener('click', handleLinkClick);
